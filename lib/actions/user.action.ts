e7a388eb658f86a8f6bcdbe107a3458fc089e09d
unknown <rahber.hussain83@gmail.com>
2023-12-18 15:39:32 +0530

    removing-console-fixing-eslint-error

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -66,9 +66,9 @@ export async function deleteUser(params: DeleteUserParams) {
     // and questions, answers, comments, etc.
 
     // get use queston ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
 
     await Question.deleteMany({ author: user._id });
 
